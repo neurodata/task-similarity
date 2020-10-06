72944ac44351a7c869a4614dcf6ac08bfaaf861c
hayden <haydenshelm@gmail.com>
2020-10-06 08:38:07 -0700

    initial PL implementation

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "proglearn",
+]
+
+with open("README.md", mode="r", encoding = "utf8") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="tasksim",
+    version="0.0.1",
+    author="Hayden Helm",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neurodata/task-similarity/",
+#    license="MIT",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Mathematics",
+#        "License :: OSI Approved :: MIT License",
+#        "Programming Language :: Python :: 3",
+#        "Programming Language :: Python :: 3.6",
+#        "Programming Language :: Python :: 3.7"
+    ],
+    install_requires=requirements,
+#    packages=find_packages(exclude=["tests", "tests.*", "tests/*"]),
+#    include_package_data=True
+)
---
+++ tasksim/tasksim.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from proglearn.transformers import TreeClassificationTransformer
+from proglearn.voters import TreeClassificationVoter
+
+def task_similarity(datax, dataz, acorn=None):
+	if acorn is not None:
+		np.random.seed(acorn)
+
+	# Initialize and fit transformers
+	transformerx = TreeClassificationTransformer()
+	transformerx.fit(datax)
+
+	transformerz = TreeClassificationTransformer()
+	transformerz.fit(dataz)
+
+	# Initialize and fit voters
+	voterx = TreeClassificationVoter()
+	voterx.fit(transformerx.transform(datax))
+
+	voterz = TreeClassificationVoter()
+	voterz.fit(transformerz.transform(datax))
+
+	# Get predictions
+	yhatx = voterx.predict(datax)
+	yhatz = voterz.predict(dataz)
+
+	task_similarity = np.mean(yhatx == yhatz)
+
+	return task_similarity
